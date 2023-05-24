2730542fb9af7a81aeceb0eccf5c288c4138124e
andrea-poletti-unibo <andrea.poletti9@unibo.it>
2023-05-24 16:02:56 +0200

    .

--- Utility_scripts/REINSTALL_OLD_PACKAGES_after_update_R_version.R
+++ Utility_scripts/REINSTALL_OLD_PACKAGES_after_update_R_version.R
@@ -2,35 +2,31 @@
 .libPaths()
 
 # grab old packages names
-old_packages <- installed.packages(lib.loc = "C:/Users/ajsi.kanapari2/AppData/Local/R/win-library/4.2")
+old_packages <- installed.packages(lib.loc = "C:/Users/andre/AppData/Local/R/win-library/4.2/")
+
 old_packages <- as.data.frame(old_packages)
+
 list.of.packages <- unlist(old_packages$Package)
 
-save(old_packages, file="workfiles/package_list_old_ajsi.Rda")
+save(old_packages, file="workfiles/package_list_andrea_24-05-23.Rda")
 
 
 # install necessary packages
-# install.packages("data.table")
 # install.packages("dplyr")
 
 library(dplyr)
-library(data.table)
 
 # carica il file
-load("workfiles/package_list_old_ajsi.Rda")
-
-# remove old packages 
-# remove.packages( installed.packages( priority = "NA" )[,1] )
+load("workfiles/package_list_andrea_24-05-23.Rda")
 
 list.of.packages <- unlist(old_packages$Package)
 
-# reinstall all packages 
+# Reinstallzione dei pacchetti da CRAN
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
 
-# resinstall old pkgs from bioconductor
-library(tidyverse)
+# Reinstallzione pacchetti da BIOCONDUCTOR
 
 list.of.packages.biocond <- old_packages %>% filter(grepl("Artistic", old_packages$License)) %>% .$Package
 BiocManager::install(list.of.packages.biocond)
@@ -43,7 +39,6 @@ BiocManager::install(failed.biocond)
 
 
 # final check
-library(tidyverse)
 final.packages <- installed.packages() %>% as.data.frame() %>% .$Package
 
 still.missing <- list.of.packages[!(list.of.packages %in% final.packages)]
@@ -51,23 +46,20 @@ still.missing <- list.of.packages[!(list.of.packages %in% final.packages)]
 BiocManager::install(still.missing)
 install.packages(still.missing)
 
-# github packages
+# pacchetti da altre fonti
 devtools::install_github('VanLoo-lab/ascat/ASCAT')
 devtools::install_github("Wedge-Oxford/dpclust")
+
 devtools::install_github("andrea-poletti-unibo/BioNerds")
+devtools::install_bitbucket('n0s3n/rawcopy/rawcopyPackage')
 
 
 
 # check dei pacchetti mancanti
 #############################################
-new.list<-as.data.frame(list.of.packages) 
 
-
-
-list.of.packages
-
-actual_packages <- installed.packages(lib.loc = "C:/Users/ajsi.kanapari2/AppData/Local/R/win-library/4.2")
+actual_packages <- installed.packages(lib.loc = "C:/Users/andre/AppData/Local/R/win-library/4.2")
 actual_packages <- actual_packages %>% as.data.frame()
 
 list.of.packages2 <- actual_packages$Package
-list.of.packages[(list.of.packages %in% list.of.packages2)==FALSE]
+list.of.packages[!(list.of.packages %in% list.of.packages2)]
