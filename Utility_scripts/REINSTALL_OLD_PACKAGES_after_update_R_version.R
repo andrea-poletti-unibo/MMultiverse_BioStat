49b84c4548eed02c79bb63a5563b26d727a41ee5
ajsikanap <ajsi.kanapari@studio.unibo.it>
2023-05-24 15:44:33 +0200

    .

--- Utility_scripts/REINSTALL_OLD_PACKAGES_after_update_R_version.R
+++ Utility_scripts/REINSTALL_OLD_PACKAGES_after_update_R_version.R
@@ -2,15 +2,27 @@
 .libPaths()
 
 # grab old packages names
-old_packages <- installed.packages(lib.loc = "C:/Users/ajsik/AppData/Local/R/win-library/4.2")
+old_packages <- installed.packages(lib.loc = "C:/Users/ajsi.kanapari2/AppData/Local/R/win-library/4.2")
 old_packages <- as.data.frame(old_packages)
 list.of.packages <- unlist(old_packages$Package)
 
-readr::write_tsv(old_packages, "workfiles/package_list_old_ajsi.txt")
 save(old_packages, file="workfiles/package_list_old_ajsi.Rda")
+
+
+# install necessary packages
+# install.packages("data.table")
+# install.packages("dplyr")
+
+library(dplyr)
+library(data.table)
+
+# carica il file
+load("workfiles/package_list_old_ajsi.Rda")
 
 # remove old packages 
 # remove.packages( installed.packages( priority = "NA" )[,1] )
+
+list.of.packages <- unlist(old_packages$Package)
 
 # reinstall all packages 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
@@ -44,3 +56,18 @@ devtools::install_github('VanLoo-lab/ascat/ASCAT')
 devtools::install_github("Wedge-Oxford/dpclust")
 devtools::install_github("andrea-poletti-unibo/BioNerds")
 
+
+
+# check dei pacchetti mancanti
+#############################################
+new.list<-as.data.frame(list.of.packages) 
+
+
+
+list.of.packages
+
+actual_packages <- installed.packages(lib.loc = "C:/Users/ajsi.kanapari2/AppData/Local/R/win-library/4.2")
+actual_packages <- actual_packages %>% as.data.frame()
+
+list.of.packages2 <- actual_packages$Package
+list.of.packages[(list.of.packages %in% list.of.packages2)==FALSE]
